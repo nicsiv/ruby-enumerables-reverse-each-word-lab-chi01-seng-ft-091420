b2edc5e753b62eadf5dd73be95b991a9b0f1cb49
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-06 01:22:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
-def reverse_each_word("Hello there, and how are you?")
-  sentence.split.map {|word| word.reverse}.join(" ")
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
